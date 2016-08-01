fae2f8d9703c858cb2c8207623fdbd2271ee8f1a
Catherine Gracey <cgracey@hotmail.com>
2016-08-01 11:11:39 +1000

    Added gulp to minify css files on post install.

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+"use strict";
+
+var  gulp = require("gulp"),
+ cleanCSS = require("gulp-clean-css"),
+  csscomb = require('gulp-csscomb'),
+  csslint = require('gulp-csslint'),
+     maps = require("gulp-sourcemaps"),
+      del = require("del");
+
+//CSS tasks
+
+gulp.task("cleanCSS", function(){
+  return del("public/css/*.css*");
+});
+
+gulp.task("combCSS", function() {
+  return gulp.src('css/*.css')
+    .pipe(csscomb())
+    .pipe(gulp.dest('dev/styles'));
+});
+
+gulp.task("lintCSS", ["combCSS"], function() {
+   return gulp.src('css/*.css')
+    .pipe(csslint())
+    .pipe(csslint.reporter());
+});
+
+gulp.task("minifyCSS", ["cleanCSS", "combCSS"], function(){
+  return gulp.src('css/*.css')
+    .pipe(maps.init())
+    .pipe(cleanCSS())
+    .pipe(maps.write("./"))
+    .pipe(gulp.dest('public/css'));
+});
+
+gulp.task("watchFiles", function(){
+  gulp.watch("css/*.css", ["lintCSS"]);
+});
+
+gulp.task("build", ["minifyCSS"]);
